2a0cef3940078d0bb29bdc6cfbbc4ad6cc28fef6
mzkx-00 <hiraeese@gmail.com>
2025-07-06 01:59:01 +0900

    FizzBuzzできた

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -15,12 +15,17 @@ const hikakin = {
 // TRY: 練習問題１
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
 // オブジェクトにはたくさんのプロパティを追加してください。
-
+const me = {
+  name: "みずき",
+  age: 23,
+  hobbies: ["ギター", "レコード鑑賞", "ドライブ"],
+}
 // const me = { name: 'あなたのなまえ' }
 
 // ブラウザのコンソールを開いてオブジェクトが正しく作れているか確認してみましょう👀
 
-// console.log(me)
+console.log(me)
 
 // TRY: 練習問題２
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+hikakin.sayHello()
