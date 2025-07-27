fb02af25fa518e0bf3cc445bed5c407ab8d2e028
mzkx-00 <hiraeese@gmail.com>
2025-07-27 15:57:16 +0900

    ジョン・レノンクイズを作った

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -3,25 +3,29 @@ const quizImage = document.getElementById("quiz-image")
 const choice1 = document.getElementById("choice-1")
 const choice2 = document.getElementById("choice-2")
 const choice3 = document.getElementById("choice-3")
+const choice4 = document.getElementById("choice-4")
 const feedback = document.getElementById("feedback")
 
 // クイズの内容
 const quiz = {
-  text: "この星の名前は何でしょう？",
-  image: "Ganymede.jpg",
+  text: "ジョン・レノンはどれでしょう？",
+  image: "Beatles.jpg",
   choices: [
     {
-      text: "ゴリアテ",
-      feedback:
-        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+      text: "1",
+      feedback: "残念！ジョージ・ハリスンだよ。",
     },
     {
-      text: "ゼニガメ",
-      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+      text: "2",
+      feedback: "正解！ジョン・レノンだよ！",
     },
     {
-      text: "ガニメデ",
-      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+      text: "3",
+      feedback: "残念！リンゴ・スターだよ。",
+    },
+    {
+      text: "4",
+      feedback: "残念！ポール・マッカートニーだよ。",
     },
   ],
 }
@@ -38,6 +42,7 @@ const reloadQuiz = function () {
   choice1.textContent = quiz.choices[0].text
   choice2.textContent = quiz.choices[1].text
   choice3.textContent = quiz.choices[2].text
+  choice4.textContent = quiz.choices[3].text
 }
 
 // choiceNumber番目の選択肢を選択
@@ -58,6 +63,9 @@ choice3.onclick = function () {
   // 2 番目の選択肢を選択
   choose(2)
 }
-
+choice4.onclick = function () {
+  // 2 番目の選択肢を選択
+  choose(3)
+}
 // reloadQuiz関数 を実行して、クイズを画面に表示する
 reloadQuiz()
